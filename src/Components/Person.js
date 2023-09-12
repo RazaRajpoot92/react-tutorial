028b94d6026e541055af08e11c9ba6f56cced692
Raza <razarajpoot99598@gmail.com>
2023-09-12 17:10:21 +0500

    First Commit

---
+++ src/Components/Person.js
@@ -0,0 +1,14 @@
+import React,{Component} from "react";
+
+class Person extends Component {
+    render(){
+        return(
+            <div className="Person">
+                <h1>Name: Root</h1>
+                <h1>Age: 19</h1>
+            </div>
+        );
+    }
+}
+
+export default Person;
