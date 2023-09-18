5e1d0e77ed8317048ab2731dd27114d357db9788
Raza <razarajpoot99598@gmail.com>
2023-09-18 11:17:22 +0500

    cleanup the app

--- src/index.js
+++ src/index.js
@@ -1,28 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import Person from "./Components/Person";
-import Man from "./Components/man"
-import Animal from "./Components/state"
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
   <div>
 
-    <Person name="john"/>
-    <App />
-    <Man count="1"/>
-    <Animal />
-
-
   </div>
 
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
